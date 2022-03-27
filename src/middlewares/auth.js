7f6d0c2b4bd710d659e9d27e52177d6f05178a4e
andrescuello7 <andrescuellotrabajo.com>
2022-03-27 11:34:56 -0300

    update master | middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -7,7 +7,7 @@ module.exports = function (req, res, next) {
     }
     try {
         const cifrado = jwt.verify(token, process.env.SECRETA);
-        req.usuario = cifrado.usuario;
+        req.user = cifrado.user;
         next();
     } catch (error) {
         res.status(401).json({msg: "error en autenticaion de token"})
